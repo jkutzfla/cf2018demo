3463476669bab65cd3dc43a86ae08bf2379d81f4
Josh Kutz-Flamenbaum <jkutzflamenbaum@gmail.com>
2018-09-22 06:03:22 -0400

    update default data

--- sql/populateDB.sql
+++ sql/populateDB.sql
@@ -2,3 +2,13 @@
 
 insert into users (lastname, firstname) values ('Kutz-Flamenbaum', 'Josh');
 insert into users (lastname, firstname) values ('User', 'Demo');
+
+insert into products (name, priceDollar, pricePoint, category) values ('Cheap Shampoo', 9.99, 0, 'Shampoo');
+insert into products (name, priceDollar, pricePoint, category) values ('Drug Store Shampoo', 14.99, 0, 'Shampoo');
+insert into products (name, priceDollar, pricePoint, category) values ('Prestige Shampoo', 28.00, 100, 'Shampoo');
+
+insert into products (name, priceDollar, pricePoint, category) values ('Cheap Conditioner', 9.99, 0, 'Conditioner');
+insert into products (name, priceDollar, pricePoint, category) values ('Drug Store Conditioner', 14.99, 0, 'Conditioner');
+insert into products (name, priceDollar, pricePoint, category) values ('Prestige Conditioner', 28.00, 100, 'Conditioner');
+
+update products set dateCreated=getdate(), dateModified=getdate();
