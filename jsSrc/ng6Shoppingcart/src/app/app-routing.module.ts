cef4bb42d9fe8c173cf07818fb59e558d1fe6b66
Josh Kutz-Flamenbaum <jkutzflamenbaum@gmail.com>
2018-10-01 09:19:21 -0400

    switch to not use hash in URL, need tuckeyreWrite

--- jsSrc/ng6Shoppingcart/src/app/app-routing.module.ts
+++ jsSrc/ng6Shoppingcart/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ShoppingcartListComponent } from './shoppingcart/shoppingcart-list/shoppingcart-list.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
 	{ path: 'cart', component: ShoppingcartListComponent},
-	{ path: '', redirectTo: 'cart', pathMatch: 'full'}
+	{ path: '', redirectTo: 'cart', pathMatch: 'full'},
+	{ path: '**', redirectTo: 'cart'}
 ];
 
 @NgModule({
 	imports: [
-		RouterModule.forRoot(routes, {enableTracing: false, useHash: true})
+		RouterModule.forRoot(routes, {enableTracing: false, useHash: false})
 	],
 	exports: [
 		RouterModule
