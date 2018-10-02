f07ea913584cf0fb746b735e9e551eabf0d13d25
Josh Kutz-Flamenbaum <jkutzflamenbaum@gmail.com>
2018-10-02 18:00:12 -0400

    add the spinner as a a shared component

---
+++ jsSrc/ng6Shoppingcart/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// TODO:
+// import { SpinnerComponent } from './spinner/spinner.component';
+
+@NgModule({
+	imports: [
+		CommonModule
+	],
+	// declarations: [SpinnerComponent],
+	// exports: [SpinnerComponent]
+})
+export class SharedModule { }
---
+++ jsSrc/ng6Shoppingcart/src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-spinner',
+	templateUrl: './spinner.component.html'
+})
+export class SpinnerComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
